6d94e408a867353752b36113b0a15ac8cb435697
Demid Greshnikov <d_greshnikov@tlen.pl>
2020-07-22 17:45:04 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+import { AboutComponent } from './about/about.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostsComponent,
+    AboutComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
